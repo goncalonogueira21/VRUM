da2792f5d7692817865a5989a6c384b3e07b2f5a
Gonçalo Borges <gborgesink@gmail.com>
2021-12-21 21:31:29 +0000

    adicionar relações

--- backend/models.py
+++ backend/models.py
@@ -19,7 +19,7 @@ class Utilizador(db.Model):
 
 class Viagem(db.Model):
     idViagem = db.Column(db.Integer, primary_key=True)
-    fk_Carro_matricula = db.Column(db.String(45))
+    fk_Carro_matricula = db.Column(db.String(45), db.ForeignKey('carro.matricula'), nullable=False)
     dataInicio = db.Column(db.DateTime)
     kmsViagem = db.Column(db.Float)
     custoPessoa = db.Column(db.Float)
@@ -35,7 +35,7 @@ class Viagem(db.Model):
 
 class Carro(db.Model):
     matricula = db.Column(db.String(45), primary_key=True)
-    fk_Utilizador_username = db.Column(db.String(45))
+    fk_Utilizador_username = db.Column(db.String(45), db.ForeignKey('utilizador.username'), nullable=False)
     modelo = db.Column(db.String(45))
     ano = db.Column(db.Integer)
     tipoFuel = db.Column(db.String(45))
@@ -46,7 +46,7 @@ class Carro(db.Model):
 
 class Avaliacao(db.Model):
     idAvaliacao = db.Column(db.Integer, primary_key=True)
-    fk_Viagem_idViagem = db.Column(db.Integer)
+    fk_Viagem_idViagem = db.Column(db.Integer, db.ForeignKey('viagem.idViagem'), nullable=False)
     conteudo = db.Column(db.String(45))
     dataAvaliacao = db.Column(db.DateTime)
     utilizador = db.Column(db.String(45))
@@ -54,19 +54,19 @@ class Avaliacao(db.Model):
 
 class Pedido(db.Model):
     idPedido = db.Column(db.Integer, primary_key=True)
-    fk_Utilizador_username = db.Column(db.String(45))
-    fk_Viagem_idViagem = db.Column(db.Integer)
+    fk_Utilizador_username = db.Column(db.String(45), db.ForeignKey('utilizador.username'), nullable=False)
+    fk_Viagem_idViagem = db.Column(db.Integer, db.ForeignKey('viagem.idViagem'), nullable=False)
     nrPessoas = db.Column(db.Integer)
     pickupLocal = db.Column(db.String(45))
     localDestino = db.Column(db.String(45))
     aceite = db.Column(TINYINT(1))
 
-'''
+
 class Usufrui(db.Model):
-    fk_Utilizador_username = db.Column(db.String(45))
-    fk_Viagem_idViagem = db.Column(db.Integer)
+    fk_Utilizador_username = db.Column(db.String(45), db.ForeignKey('utilizador.username'), nullable=False)
+    fk_Viagem_idViagem = db.Column(db.Integer, db.ForeignKey('viagem.idViagem'), nullable=False)
     custoPago = db.Column(db.Float)
-'''
+
 
 class Mensagem(db.Model):
     idMensagem = db.Column(db.Integer, primary_key=True)
@@ -74,3 +74,9 @@ class Mensagem(db.Model):
     userOrigem = db.Column(db.String(45))
     userDestino = db.Column(db.String(45))
     data = db.Column(db.DateTime)
+
+class mailBox(db.Model):
+    idMailBox = db.Column(db.Integer, primary_key=True)
+    fk_Utilizador_username = db.Column(db.String(45), db.ForeignKey('utilizador.username'), nullable=False)
+    fk_Mensagens_idMensagens = db.Column(db.Integer, db.ForeignKey('mensagem.idMensagem'), nullable=False)
+    mailbox = db.Column(db.String(45))
